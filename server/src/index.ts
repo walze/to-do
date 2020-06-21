5809edbff113da0bd69f68dc62ce0552ba6e6541
walze <catufuzgu@gmail.com>
2020-06-21 12:25:18 -0300

    added resolver

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,31 @@
+import { addResolversToSchema } from '@graphql-tools/schema'
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
+import { } from 'graphql-tools'
 
-const a: string = 'asdf'
+const hello = (...args) => {
+  console.log({ ...args })
 
-console.log(a)
+  return 'nice'
+}
+// Write some resolvers
+const resolvers = {
+  Query: {
+    hello
+  }
+}
+
+const app = express()
+
+app.use(
+  graphqlHTTP({
+    // Add resolvers to the schema
+    schema: addResolversToSchema({ schema, resolvers }),
+    graphiql: true
+  })
+)
+
+app.listen(4000, () => {
+  console.info('Server listening on http://localhost:4000')
+})
