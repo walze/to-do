a665f25e7e186a41f0c6a14e0b4a9e04ee0ac49c
walze <catufuzgu@gmail.com>
2020-07-07 14:30:41 -0300

    deleted files

--- src/helpers/saveEntity.ts
+++
@@ -1,4 +0,0 @@
-import { Connection } from 'typeorm'
-
-export const saveEntity = (conn: Connection) => <T>(data: T) =>
-  conn.manager.save(data)
--- src/models/User.ts
+++
@@ -1,8 +0,0 @@
-
-import { UserInput } from 'app/generated/graphql'
-import { Prisma } from 'app/helpers/useConnection'
-
-export const createUser = (conn: Prisma) =>
-  ({ name }: UserInput) => conn.user.create({
-    data: { name }
-  })
