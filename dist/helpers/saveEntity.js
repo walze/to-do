588f698e02e2cad8376562aa746bc2acf563a9cd
walze <catufuzgu@gmail.com>
2020-07-07 10:30:48 -0300

    removed unsusaed

--- dist/helpers/saveEntity.js
+++
@@ -1,5 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _useConnection = require('../helpers/useConnection');
-
- const saveEntity = (data) => _useConnection.useConnection.call(void 0, 
-    (conn) => conn.manager.save(data),
-); exports.saveEntity = saveEntity;
